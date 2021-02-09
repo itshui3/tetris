75535fff816448824e2d26f307d98cdc124454f0
Hui <xuhuizhu369@gmail.com>
2021-02-08 19:07:07 -0800

    fixes lineDrop

--- src/components/Tetris/assets/clearLines.js
+++ src/components/Tetris/assets/clearLines.js
@@ -2,16 +2,32 @@
 
 const clearLines = (board, rows) => {
     console.log(board, rows)
-    const updatedBoard = board.map((row, r_idx) => {
-        if (rows.has(r_idx)) {
-            return new Array(board[0].length).fill(0)
+    // const updatedBoard = board.map((row, r_idx) => {
+    //     if (rows.has(r_idx)) {
+    //         return new Array(board[0].length).fill(0)
+    //     } else {
+    //         return row
+    //     }
+    // })
+
+    const draftBoard = new Array(board.length)
+
+    let floor = board.length-1
+    for (let r = board.length-1; r > -1; r--) {
+        if (rows.has(r)) {
+            console.log('has r')
+            // do nothing
         } else {
-            return row
+            draftBoard[floor] = board[r]
+            floor -= 1
         }
-    })
+    }
 
-    return updatedBoard
-
+    for (let i = 0; i < rows.size; i++) {
+        draftBoard[i] = new Array(board[0].length).fill(0)
+    }
+    console.log('draftBoard', draftBoard)
+    return draftBoard
 }
 
 export { clearLines }
