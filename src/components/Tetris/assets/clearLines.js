d5101ddfa18d306e37837135da791dbc1689e95f
Hui <xuhuizhu369@gmail.com>
2021-02-08 19:10:59 -0800

    simplifies clearLine logic

--- src/components/Tetris/assets/clearLines.js
+++ src/components/Tetris/assets/clearLines.js
@@ -14,19 +14,17 @@ const clearLines = (board, rows) => {
 
     let floor = board.length-1
     for (let r = board.length-1; r > -1; r--) {
-        if (rows.has(r)) {
-            console.log('has r')
-            // do nothing
-        } else {
+        if (!rows.has(r)) {
             draftBoard[floor] = board[r]
             floor -= 1
+            // do nothing
         }
     }
 
     for (let i = 0; i < rows.size; i++) {
         draftBoard[i] = new Array(board[0].length).fill(0)
     }
-    console.log('draftBoard', draftBoard)
+
     return draftBoard
 }
 
