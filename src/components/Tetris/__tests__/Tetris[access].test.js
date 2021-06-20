03c03d50da1c35135a0df1df18286b190f6381e6
Hui <xuhuizhu369@gmail.com>
2021-06-20 12:34:46 -0700

    tests that start game & kill pc buttons render

--- src/components/Tetris/__tests__/Tetris[access].test.js
+++ src/components/Tetris/__tests__/Tetris[access].test.js
@@ -5,8 +5,11 @@ import { screen, render } from '@testing-library/react';
 
 import Tetris from '../Tetris';
 
-test('it renders Tetris component', () => {
+beforeEach(() => {
     render(<Tetris />);
+});
+
+test('it renders Tetris component', () => {
 
     const dom_TetrisCont = screen.getByTestId('tetris_cont');
 
@@ -14,7 +17,6 @@ test('it renders Tetris component', () => {
 });
 
 test('it renders a board with the right number of cols/rows', () => {
-    render(<Tetris />);
 
     const dom_TetrisCont = screen.getByTestId('tetris_cont');
 
@@ -22,4 +24,17 @@ test('it renders a board with the right number of cols/rows', () => {
     expect(dom_TetrisCont.children.length).toBe(25);
     // cols
     expect(dom_TetrisCont.children[0].children.length).toBe(10);
+});
+
+test('it renders start button', () => {
+
+    const dom_StartBtn = screen.getByText('Start Game');
+
+    expect(dom_StartBtn).toBeInTheDocument();
+});
+
+test('it renders kill active piece button', () => {
+    const dom_KillPcBtn = screen.getByText('kill active pc');
+
+    expect(dom_KillPcBtn).toBeInTheDocument();
 });
