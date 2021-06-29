cda5d4fc99da502e340f7072c55eebb7c9cc725f
Hui <xuhuizhu369@gmail.com>
2021-06-29 15:36:37 -0700

    tests that active piece can't move further right when against right wall

--- src/components/Tetris/__tests__/Tetris[movementCollision].test.js
+++ src/components/Tetris/__tests__/Tetris[movementCollision].test.js
@@ -31,6 +31,7 @@ beforeEach(() => {
     
 });
 
+// can I simplify this? 
 test('active pc stops when reaching left wall', () => {
     const dom_leftCtrl = screen.getByTestId('control_left');
 
@@ -59,6 +60,56 @@ test('active pc stops when reaching left wall', () => {
     }
 
     fireEvent.click(dom_leftCtrl);
+
+    let postfireCoords = new Set();
+
+    for (let r = 0; r < dom_tetrisCont.children.length; r++) {
+
+        for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
+            if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
+                postfireCoords.add(`${r}.${c}`);
+            }
+
+        }
+
+    }
+
+
+    prefireCoords.forEach(pre => {
+        expect(postfireCoords.has(pre[0]+'.'+pre[1])).toBeTruthy();
+    });
+
+});
+
+// 
+test('active pc stops when reaching right wall', () => {
+    const dom_rightCtrl = screen.getByTestId('control_right');
+
+    let maxY = 0;
+
+    for (let i = 0; i < preAdjustCoords.length; i++) {
+        let yCoord = preAdjustCoords[i][1];
+        if (yCoord > maxY) maxY = yCoord;
+    }
+
+    for (let i = 0; i < 9-maxY; i++) {
+        fireEvent.click(dom_rightCtrl);
+    }
+
+    let prefireCoords = [];
+
+    for (let r = 0; r < dom_tetrisCont.children.length; r++) {
+
+        for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
+            if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
+                prefireCoords.push([r, c]);
+            }
+
+        }
+
+    }
+
+    fireEvent.click(dom_rightCtrl);
 
     let postfireCoords = new Set();
 
