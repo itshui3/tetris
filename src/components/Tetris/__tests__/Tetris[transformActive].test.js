8f079c85bbda6bfb4397c56987debee4d06a0ef1
Hui <xuhuizhu369@gmail.com>
2021-06-30 13:14:58 -0700

    sets up active pc one row above bottom for transform tests

--- src/components/Tetris/__tests__/Tetris[transformActive].test.js
+++ src/components/Tetris/__tests__/Tetris[transformActive].test.js
@@ -1,101 +1,98 @@
 
-// tests that mid-game keystrokes function correctly
 import React from 'react';
 
 import { render, screen, fireEvent } from '@testing-library/react';
 
 import Tetris from '../Tetris';
 
-// handleKeys={['f', 'd', 's', 'r', 'w']}
-// which coordinates are occupied? 
-let prefireCoords = [];
-let dom_tetrisCont;
+let shift = NaN; 
+
+let dom_tetrisCont
+let dom_downCtrl;
 
 beforeEach(() => {
-    prefireCoords = [];
-
+// [] - start game
     render(<Tetris />);
 
     const dom_startGame = screen.getByTestId('startGame');
     fireEvent.click(dom_startGame);
+// [] - place active pc 1 unit above lowest col
+    let spawnCoords = [];
+    let lowest = 0;
 
     dom_tetrisCont = screen.getByTestId('tetris_cont');
     for (let r = 0; r < dom_tetrisCont.children.length; r++) {
 
         for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
             if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
-                prefireCoords.push([r, c]);
+                spawnCoords.push([r, c]);
+                if (r > lowest) lowest = r;
             }
 
         }
 
     }
+
+    shift = 22-lowest;
+
+    dom_downCtrl = screen.getByTestId('control_down');
+
+    for (let i = 0; i < shift; i++) {
+        fireEvent.click(dom_downCtrl);
+    }
+    
 });
 
-test('keyboard press \'s\' moves block to the left', () => {
-    const dom_leftCtrl = screen.getByTestId('control_left');
-    fireEvent.click(dom_leftCtrl);
-
-    let postfireCoords = new Set();
-
+test('it sets up active pc one row above bottom', () => {
+    let coords = [];
+    let lowest = 0;
     for (let r = 0; r < dom_tetrisCont.children.length; r++) {
 
         for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
             if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
-                postfireCoords.add(`${r}.${c}`);
+                coords.push([r, c]);
+                if (r > lowest) lowest = r;
             }
 
         }
 
     }
 
-    prefireCoords.forEach(pre => {
-        expect(postfireCoords.has(pre[0]+'.'+(pre[1]-1))).toBeTruthy();
-    });
-
+    expect(lowest).toBe(22);
 });
 
-test('keyboard press \'d\' moves block to the south', () => {
-    const dom_downCtrl = screen.getByTestId('control_down');
-    fireEvent.click(dom_downCtrl);
+test('it drops active pc to bottom row', () => {
+    let coords = [];
 
-    let postfireCoords = new Set();
-
+    let lowest = 0;
     for (let r = 0; r < dom_tetrisCont.children.length; r++) {
 
         for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
             if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
-                postfireCoords.add(`${r}.${c}`);
+                coords.push([r, c]);
+                if (r > lowest) lowest = r;
             }
 
         }
 
     }
-
-    prefireCoords.forEach(pre => {
-        expect(postfireCoords.has((pre[0]+1)+'.'+pre[1])).toBeTruthy();
-    });
-
+    
 });
 
-test('keyboard press \'f\' moves block to the right', () => {
-    const dom_rightCtrl = screen.getByTestId('control_right');
-    fireEvent.click(dom_rightCtrl);
+test('on down action from bottom row spawns a new active pc', () => {
+    let coords = [];
 
-    let postfireCoords = new Set();
-
+    let lowest = 0;
     for (let r = 0; r < dom_tetrisCont.children.length; r++) {
 
         for (let c = 0; c < dom_tetrisCont.children[r].children.length; c++) {
             if (!!dom_tetrisCont.children[r].children[c].style.backgroundColor) {
-                postfireCoords.add(`${r}.${c}`);
+                coords.push([r, c]);
+                if (r > lowest) lowest = r;
             }
 
         }
 
     }
 
-    prefireCoords.forEach(pre => {
-        expect(postfireCoords.has(pre[0]+'.'+(pre[1]+1))).toBeTruthy();
-    });
 });
