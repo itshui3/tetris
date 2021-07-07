08b73bf6e556a65da19497fbb69cd76a3e40bcac
Hui <xuhuizhu369@gmail.com>
2021-07-06 17:40:31 -0700

    refactors basic rotation tests to read more cleanly

--- src/components/Tetris/__tests__/Tetris[rotateBasic].test.js
+++ src/components/Tetris/__tests__/Tetris[rotateBasic].test.js
@@ -7,22 +7,26 @@ import { getPcs } from '../../../helpers/spec/getPcs';
 
 import { initBoard, dummyPcs } from '../useTetrisHooks';
 
-const verticalTriPc = [ '0.5', '1.5', '2.5' ];
-const horizontalTriPc = ['1.4', '1.5', '1.6'];
+const pcs = {
+    verticalTriPc: [ '0.5', '1.5', '2.5' ],
+    horizontalTriPc: ['1.4', '1.5', '1.6']
+};
 
-test('it rotates active pc ccw', () => {
+beforeEach(() => {
     render(<Tetris initBoard={initBoard(dummyPcs)} />);
 
     const dom_startGame = screen.getByTestId('startGame');
     fireEvent.click(dom_startGame);
+});
 
+test('it rotates active pc ccw', () => {
     const dom_tetris = screen.getByTestId('tetris_cont');
 
     const dom_ccw = screen.getByTestId('control_ccw');
 
     const before = getPcs(dom_tetris);
 
-    verticalTriPc.forEach(v_pc => {
+    pcs.verticalTriPc.forEach(v_pc => {
         expect(before.activePcsSet).toContain(v_pc);
     });
 
@@ -30,32 +34,27 @@ test('it rotates active pc ccw', () => {
     fireEvent.click(dom_ccw);
     const after = getPcs(dom_tetris);
 
-    horizontalTriPc.forEach(h_pc => {
+    pcs.horizontalTriPc.forEach(h_pc => {
         expect(after.activePcsSet).toContain(h_pc);
     });
 
 });
 
 test('it rotates piece cw', () => {
-    render(<Tetris initBoard={initBoard(dummyPcs)} />);
-
-    const dom_startGame = screen.getByTestId('startGame');
-    fireEvent.click(dom_startGame);
-
     const dom_tetris = screen.getByTestId('tetris_cont');
 
     const dom_cw = screen.getByTestId('control_cw');
 
     const before = getPcs(dom_tetris);
 
-    verticalTriPc.forEach(v_pc => {
+    pcs.verticalTriPc.forEach(v_pc => {
         expect(before.activePcsSet).toContain(v_pc);
     });
 
     fireEvent.click(dom_cw);
     const after = getPcs(dom_tetris);
 
-    horizontalTriPc.forEach(h_pc => {
+    pcs.horizontalTriPc.forEach(h_pc => {
         expect(after.activePcsSet).toContain(h_pc);
     });
 
