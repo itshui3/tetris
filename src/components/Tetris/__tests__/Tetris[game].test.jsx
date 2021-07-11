514ec9f19081d4c91fad3e5eb961cef739261a41
Hui <xuhuizhu369@gmail.com>
2021-07-10 17:51:45 -0700

    tests that starting tetris should spawn an active pc

--- src/components/Tetris/__tests__/Tetris[game].test.jsx
+++ src/components/Tetris/__tests__/Tetris[game].test.jsx
@@ -5,25 +5,40 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import Tetris from '../Tetris';
 
+import { initBoard, gamePcs } from '../useTetrisHooks';
+
+import { getPcs } from '../../../helpers/spec/getPcs';
+
 beforeEach(() => {
-    render(<Tetris />);
+    render(<Tetris initBoard={initBoard(gamePcs)} />);
 });
 
 //=====================
 // [] - startGame suite
-test('starting tetris should spawn an active pc', () => null);
-// only an ended && reset game would have no pcs in play
-test('tetris should only start if there are no pcs in play', () => null);
+test('starting tetris should spawn an active pc', () => {
+    const dom_tetris = screen.getByTestId('tetris_cont');
 
-//=====================
+    const prefirePcs = getPcs(dom_tetris);
+    expect(prefirePcs.activePcsList.length).toBe(0);
+
+    const dom_startGame = screen.getByTestId('startGame');
+    fireEvent.click(dom_startGame);
+
+    const postfirePcs = getPcs(dom_tetris);
+    expect(postfirePcs.activePcsList.length).toBeGreaterThanOrEqual(1);
+});
+// only an ended && reset game would have no pcs in play
+test('tetris should only start if there are no pcs in play', () => undefined);
+
+//====================
 // [] - endGame suite
-test('ending tetris should freeze active pc and prevent movement', () => null);
+test('ending tetris should freeze active pc and prevent movement', () => undefined);
 // vvv - leave this one for later, when I start doing drop interval testing
-test('ending tetris should stop active pc from dropping asynchronously', () => null);
+test('ending tetris should stop active pc from dropping asynchronously', () => undefined);
 
 //=====================
 // [] - resetGame suite
-test('tetris resets only if game is ended', () => null);
+test('tetris resets only if game is ended', () => undefined);
 // if there are no pcs, either the board has already been reset or no games have been played yet
-test('tetris resets only if pieces are leftover', () => null);
-test('tetris resets wipe active/static pcs', () => null);
+test('tetris resets only if pieces are leftover', () => undefined);
+test('tetris resets wipe active/static pcs', () => undefined);
