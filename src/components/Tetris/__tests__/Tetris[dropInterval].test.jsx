514623b66119bedec2d3c70d61fab1969b53b618
Hui <xuhuizhu369@gmail.com>
2021-07-26 04:19:38 -0700

    tests that active pieces drop on interval!

--- src/components/Tetris/__tests__/Tetris[dropInterval].test.jsx
+++ src/components/Tetris/__tests__/Tetris[dropInterval].test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
 
 import Tetris from '../Tetris';
 
@@ -13,11 +13,22 @@ beforeEach(() => {
 
 //=====================
 // [] - dropInterval suite
-test('starting tetris should provoke invocations of drop async', () => {
+test('starting tetris should provoke invocations of drop async', async () => {
     // dependencies
     const dom_tetris = screen.getByTestId('tetris_cont');
     const dom_startGame = screen.getByTestId('startGame');
 
     // [0] - start game
     fireEvent.click(dom_startGame);
+
+    const before = getPcs(dom_tetris);
+
+    // [1] - waitFor dropInterval to pass
+    await waitFor(() => {
+    // [2] - fetch pcs within async
+        const after = getPcs(dom_tetris);
+        const after_activePcs = after.activePcsList.map(pc => [pc[0]-1, pc[1]]);
+    // [3] - test that active pieces dropped
+        expect(before.activePcsList).toEqual(after_activePcs);
+    });
 });
